849fbbce420106e46d13248c60d220d3745298dc
MathMatowicki <matowicki@gmail.com>
2023-10-05 14:00:49 +0200

    Updated variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -5,3 +5,15 @@ variable "location" {
 variable "rg-name" {
   description = "The name of the resource group"
 }
+
+variable "storage-account-name" {
+  description = "The name of Azure storage account"
+}
+
+variable "source_content" {
+  description = "information source content"
+}
+
+variable "index_document" {
+  description = "name of file to the source content"
+}
