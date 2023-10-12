fba32d88c0d9e63c66db15bc2bf99526c302bdae
MathMatowicki <matowicki@gmail.com>
2023-10-12 09:51:54 +0200

    Edited main.tf rg variable

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage-account-name
-  resource_group_name      = azurerm_resource_group.resource_group.name
+  resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
   location                 = var.location
   account_replication_type = "LRS"
